c37cccd9f6bbd74b7979bac4a0db8f11daac7217
zuber12354 <aurri.cba@gmail.com>
2026-09-14 18:22:53 +0300

    Update test in feature branch

--- second_largest.py
+++ second_largest.py
@@ -9,6 +9,6 @@ def second_largest(numbers: list[float]) -> float:
 print(second_largest([3, 1, 4, 1, 5, 9, 2]))  # 5
 
 # 3 Test Cases for second_largest
-print(second_largest([10, 20, 30, 40]))  # Expected output: 30
+print("branch test")
 print(second_largest([5, 5, 5, 2, 1]))  # Expected output: 2
 print(second_largest([-10, -5, -20]))  # Expected output: -10
